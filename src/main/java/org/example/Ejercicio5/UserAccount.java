b1e51c96100c188786c0aa53eac99f07c6332a50
Pizarman13 <146822082+Pizarman13@users.noreply.github.com>
2024-05-08 11:40:35 +0200

    creacion clase UserAccount
    Y separacion de ejercicios 4 y 5

---
+++ src/main/java/org/example/Ejercicio5/UserAccount.java
@@ -0,0 +1,4 @@
+package org.example.Ejercicio5;
+
+public class UserAccount {
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,17 +2,44 @@ package org.example;
 
 import org.example.Ejercicio4.Recursividad;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
 
-        System.out.println("Ejercicio 4");
-        int num = 123;
-        System.out.println("La suma de los digitos de " + num + " es: " + Recursividad.recursividad(num));
+        boolean salir = true;
 
-        num = 1234;
-        System.out.println("La suma de los digitos de " + num + " es: " + Recursividad.recursividad(num));
+        while (salir) {
+            System.out.println("1. Ejercicio 4");
+            System.out.println("2. Ejercicio 5");
+            System.out.println("0. Salir");
+            System.out.print("Selecciones una opción: ");
 
+            int option = sc.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println();
+                    System.out.println("Ejercicio 4");
+                    int num = 123;
+                    System.out.println("La suma de los dígitos de " + num + " es: " + Recursividad.recursividad(num));
+                    num = 1234;
+                    System.out.println("La suma de los dígitos de " + num + " es: " + Recursividad.recursividad(num));
+                    salir = false;
+                    break;
+                case 2:
+                    System.out.println("Ejercicio 5");
+                    break;
+                case 0:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+                    break;
+            }
+        }
     }
 }
